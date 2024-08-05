7d972b284e5cdab59781e362f1b53d0a076f3c5f
isamammadli570 <144543931+isamammadli570@users.noreply.github.com>
2024-08-06 00:44:20 +0400

    initial commit

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -31,27 +31,24 @@ function Detail() {
         <div className="w-full max-w-7xl md:m-[100px] border shadow-2xl ">
           {details?.map((detail) => {
             return (
-              <div key={detail.idMeal}>
+              <div
+                className="flex justify-evenly flex-wrap p-[30px]"
+                key={detail.idMeal}
+              >
                 <div className="max-w-lg min-w-72 overflow-hidden m-[25px] ">
                   <img
-                    className="w-full h-full max-h-96 block object-cover"
+                    className="w-[100rem] h-[100rem] max-h-96 block object-cover"
                     src={detail.strMealThumb}
                     alt=""
                   />
                 </div>
                 <div className="max-w-lg min-w-72 m-[25px]">
-                  <div className="mb-[15px]">
-                    <h2>
-                      <span>Name:</span> {detail.strMeal}
-                    </h2>
-                    <div>
-                      <p>
-                        <span>Category:</span> {detail.strCategory}
-                      </p>
-                      <p>
-                        <span>Area:</span> {detail.strCategory}
-                      </p>
-                    </div>
+                  <div className="mb-[15px] ">
+                    <h2 className="text-4xl text-gray-700">{detail.strMeal}</h2>
+                    <p className="text-green-800">
+                      {detail.strCategory} / {detail.strArea}
+                    </p>
+                    <p className="py-4 text-sm">{detail.strInstructions}</p>
                   </div>
                 </div>
               </div>
